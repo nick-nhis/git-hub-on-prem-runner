da60c7c7921ce8f5ba474e7f3d08ae418d13798f
Nick Charlesworth <nick.charlesworth@notts-his.nhs.uk>
2025-11-19 09:27:32 +0000

    Add XUnit Test project

---
+++ GitHubActionsOnPremRazorPages.Tests/IndexPageTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System.Net;
+
+namespace GitHubActionsOnPremRazorPages.Tests
+{
+    public class IndexPageTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+
+        // to set the test.runsettings file via command line
+        // could be useful if need to include in CI/CD pipeline
+        //  dotnet test --settings test.runsettings
+
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public IndexPageTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task PageLoadsSuccessfully()
+        {
+            var client = _factory.CreateClient();
+            var response = await client.GetAsync("/");
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.True(response.Content.Headers.ContentType?.MediaType == "text/html");
+        }
+
+        [Fact]
+        public async Task PageContainsWelcomeHeading()
+        {
+            var client = _factory.CreateClient();
+            var response = await client.GetAsync("/");
+            var content = await response.Content.ReadAsStringAsync();
+            Assert.Contains("<h1>Welcome</h1>", content);
+        }
+    }
+}
